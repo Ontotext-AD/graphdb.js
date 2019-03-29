1c98af4315f1b65da28b1c5d842d05b6e4547451
Svilen Velikov <svilen.velikov@sirma.bg>
2019-03-29 19:04:43 +0200

    Implement more tests for the server class

--- src/tests/server.test.js
+++ src/tests/server.test.js
@@ -1,9 +1,26 @@
 // import axios from 'axios';
 import Server from '../classes/server';
+import {ServerConfig} from '../configs/server-config';
 
 jest.mock('axios');
 
-test('New server instance should not return null', (done) => {
-  expect(new Server(null)).not.toBeNull();
-  done();
+describe('Server', () => {
+  test('new Server instance should not return null', () => {
+    expect(new Server(null)).not.toBeNull();
+  });
+
+  test('should init server with required class member fields', () => {
+    const config = new TestServerConfig('server/url', {});
+    const server = new Server(config);
+    expect(server.config).toEqual({
+      url: 'server/url',
+      defaultRepositoryConfig: {},
+    });
+  });
 });
+
+/**
+ * Test implementation for the {@link ServerConfig}
+ */
+class TestServerConfig extends ServerConfig {
+}
