e57459d7d6feca50e7d005e8751dd7ec7a6e322c
desislava <desislava.hristova@ontotext.com>
2019-04-01 14:10:11 +0300

    Merge tests from two different branches

--- src/tests/server.test.js
+++ src/tests/server.test.js
@@ -6,20 +6,6 @@ jest.mock('axios', ()=> {
   require('./axios-mock');
 });
 
-test('New server instance should not return null', (done) => {
-  expect(new Server(null, {})).not.toBeNull();
-  done();
-});
-
-test('Call axios with endpoint and method at getRepositoryIDs', (done) => {
-  mockAxios.get.mockImplementationOnce(() =>{
-    Promise.resolve({
-      data: {},
-    });
-  });
-  expect(mockAxios.get).toHaveBeenCalledTimes(1);
-  expect(mockAxios.get).toHaveBennCalledWith('http://ff-dev.ontotext.com/repositories');
-  done();
 describe('Server', () => {
   test('new Server instance should not return null', () => {
     expect(new Server(null)).not.toBeNull();
@@ -33,7 +19,26 @@ describe('Server', () => {
       defaultRepositoryConfig: {},
     });
   });
+
+  test('New server instance should not return null', (done) => {
+    expect(new Server(null, {})).not.toBeNull();
+    done();
+  });
+
+  test('Call axios with endpoint and method at getRepositoryIDs', (done) => {
+    mockAxios.get.mockImplementationOnce(() =>{
+      Promise.resolve({
+        data: {},
+      });
+    });
+    expect(mockAxios.get).toHaveBeenCalledTimes(1);
+    expect(mockAxios.get).toHaveBennCalledWith('http://ff-dev.ontotext.com/repositories');
+    done();
+  });
 });
+
+
+
 
 /**
  * Test implementation for the {@link ServerConfig}
