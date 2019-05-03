d40eb8dcfa78b5b64b0a9ec1078acaf81856585d
Svilen Velikov <svilen.velikov@sirma.bg>
2019-05-03 18:07:33 +0300

    Support x-request-id for every request
    * Introduced a uuid library dependency
    * Add x-request-id header to every request
    * Implemented tests

--- test/http/http-client.spec.js
+++ test/http/http-client.spec.js
@@ -3,6 +3,8 @@ const HttpClient = require('http/http-client');
 
 jest.mock('axios');
 
+const X_REQUEST_ID_HEADER = 'x-request-id';
+
 /*
  * Tests the wrapping of axios in HttpClient and how it delegates requests.
  */
@@ -11,13 +13,9 @@ describe('HttpClient', () => {
   let axiosMock;
   let httpClient;
 
-  let requestConfig = {
-    params: {
-      'param-1': 'value-1'
-    }
-  };
+  let requestConfig;
 
-  let requestData = {
+  const requestData = {
     'property': 'payload'
   };
 
@@ -35,6 +33,12 @@ describe('HttpClient', () => {
     axiosMock.delete.mockResolvedValue();
     axiosMock.defaults = {};
 
+    requestConfig = {
+      params: {
+        'param-1': 'value-1'
+      }
+    };
+
     httpClient = new HttpClient('/base/url', 1000);
   });
 
@@ -46,37 +50,68 @@ describe('HttpClient', () => {
   });
 
   test('should allow to set default request headers', () => {
-    let headers = {'Accept': 'application/json'};
+    const headers = {'Accept': 'application/json'};
     httpClient.setDefaultHeaders(headers);
     expect(axiosMock.defaults.headers).toEqual(headers);
   });
 
-  test('should perform GET requests with the supplied params', () => {
-    return httpClient.get('/api/resources', requestConfig).then(() => {
-      expect(axiosMock.get).toHaveBeenCalledTimes(1);
-      expect(axiosMock.get).toHaveBeenCalledWith('/api/resources', requestConfig);
+  describe('GET', () => {
+    test('should perform GET requests with the supplied params', () => {
+      return httpClient.get('/api/resources', requestConfig).then(() => {
+        expect(axiosMock.get).toHaveBeenCalledTimes(1);
+        expect(axiosMock.get).toHaveBeenCalledWith('/api/resources', requestConfig);
+      });
+    });
+
+    test('should add x-request-id header', () => {
+      return httpClient.get('/api/resources', requestConfig).then(() => {
+        expect(axiosMock.get.mock.calls[0][1].headers).toHaveProperty(X_REQUEST_ID_HEADER);
+      });
     });
   });
 
-  test('should perform POST requests with the supplied params and data', () => {
-    return httpClient.post('/api/resources', requestData, requestConfig).then(() => {
-      expect(axiosMock.post).toHaveBeenCalledTimes(1);
-      expect(axiosMock.post).toHaveBeenCalledWith('/api/resources', requestData, requestConfig);
+  describe('POST', () => {
+    test('should perform POST requests with the supplied params and data', () => {
+      return httpClient.post('/api/resources', requestData, requestConfig).then(() => {
+        expect(axiosMock.post).toHaveBeenCalledTimes(1);
+        expect(axiosMock.post).toHaveBeenCalledWith('/api/resources', requestData, requestConfig);
+      });
+    });
+
+    test('should add x-request-id header', () => {
+      return httpClient.post('/api/resources', requestData, requestConfig).then(() => {
+        expect(axiosMock.post.mock.calls[0][2].headers).toHaveProperty(X_REQUEST_ID_HEADER);
+      });
     });
   });
 
-  test('should perform PUT requests with the supplied params and data', () => {
-    return httpClient.put('/api/resources/1/', requestData, requestConfig).then(() => {
-      expect(axiosMock.put).toHaveBeenCalledTimes(1);
-      expect(axiosMock.put).toHaveBeenCalledWith('/api/resources/1/', requestData, requestConfig);
+  describe('PUT', () => {
+    test('should perform PUT requests with the supplied params and data', () => {
+      return httpClient.put('/api/resources/1/', requestData, requestConfig).then(() => {
+        expect(axiosMock.put).toHaveBeenCalledTimes(1);
+        expect(axiosMock.put).toHaveBeenCalledWith('/api/resources/1/', requestData, requestConfig);
+      });
+    });
+
+    test('should add x-request-id header', () => {
+      return httpClient.put('/api/resources', requestData, requestConfig).then(() => {
+        expect(axiosMock.put.mock.calls[0][2].headers).toHaveProperty(X_REQUEST_ID_HEADER);
+      });
     });
   });
 
-  test('should perform DELETE requests with the supplied params', () => {
-    return httpClient.deleteResource('/api/resources/1/', requestConfig).then(() => {
-      expect(axiosMock.delete).toHaveBeenCalledTimes(1);
-      expect(axiosMock.delete).toHaveBeenCalledWith('/api/resources/1/', requestConfig);
+  describe('DELETE', () => {
+    test('should perform DELETE requests with the supplied params', () => {
+      return httpClient.deleteResource('/api/resources/1/', requestConfig).then(() => {
+        expect(axiosMock.delete).toHaveBeenCalledTimes(1);
+        expect(axiosMock.delete).toHaveBeenCalledWith('/api/resources/1/', requestConfig);
+      });
+    });
+
+    test('should add x-request-id header', () => {
+      return httpClient.deleteResource('/api/resources', requestConfig).then(() => {
+        expect(axiosMock.delete.mock.calls[0][1].headers).toHaveProperty(X_REQUEST_ID_HEADER);
+      });
     });
   });
-
 });
