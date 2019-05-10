768b854565dea02fd32c644b216545fd16e89542
Mihail Radkov <mihail.radkov@sirma.bg>
2019-05-10 15:32:50 +0300

    Moved statement methods from the base repo client to the RDF repo client.
    Done to ensure that the transactional client will not have to override
    uch methods from the base client.

--- src/repository/base-repository-client.js
+++ src/repository/base-repository-client.js
@@ -3,8 +3,6 @@ const ConsoleLogger = require('logging/console-logger');
 const HttpClient = require('http/http-client');
 const RepositoryClientConfig = require('repository/repository-client-config');
 const Iterable = require('util/iterable');
-const RDFMimeType = require('http/rdf-mime-type');
-const TermConverter = require('model/term-converter');
 
 /**
  * Set of HTTP status codes for which requests could be re-attempted.
@@ -14,13 +12,6 @@ const TermConverter = require('model/term-converter');
 const RETRIABLE_STATUSES = [
   503 // Server busy
 ];
-
-/**
- * Defines a path segment for statements rest endpoint
- *
- * @type {string}
- */
-const PATH_STATEMENTS = '/statements';
 
 /**
  * Implementation of the RDF repository operations.
@@ -88,78 +79,6 @@ class BaseRepositoryClient {
   }
 
   /**
-   * Retrieves the size of the repository.
-   *
-   * Effectively returns how much statements are in the repository.
-   *
-   * If one or multiple context are provided, the operation will be restricted
-   * upon each of them.
-   *
-   * @param {string|string[]} [context] context or contexts to restrict the
-   *                            size calculation
-   * @return {Promise<number>} a promise resolving to the total number of
-   *                           statements in the repository
-   */
-  getSize(context) {
-    return this.execute((http) => http.get('/size', http.getConfigBuilder()
-      .setTimeout(this.repositoryClientConfig.readTimeout)
-      .setParams({context})
-      .get())).then((response) => response.data);
-  }
-
-  /**
-   * Fetch rdf data from statements endpoint using provided parameters.
-   *
-   * @param {Object} params is an object holding request parameters as returned
-   *                 by {@link GetStatementsPayload#get()}
-   * @return {Promise<string|Quad>} resolves with plain string or Quad according
-   *      to provided response type.
-   */
-  get(params) {
-    return this.execute((http) => {
-      return http.get(PATH_STATEMENTS, http.getConfigBuilder()
-        .setParams({
-          subj: params.subject,
-          pred: params.predicate,
-          obj: params.object,
-          context: params.context,
-          infer: params.inference
-        })
-        .addAcceptHeader(params.responseType)
-        .setTimeout(this.repositoryClientConfig.readTimeout)
-        .get());
-    }).then((response) => {
-      return this.parse(response.data, params.responseType);
-    });
-  }
-
-  /**
-   * Executes request to query a repository.
-   *
-   * Only POST request with a valid QueryPayload is supported.
-   *
-   * @param {QueryPayload} payload is an object holding request parameters
-   * required by the query POST endpoint.
-   * @return {Promise} the client can subscribe to the stream events and consume
-   * the emitted strings or Quads depending on the provided response type as
-   * soon as they are available.
-   */
-  query(payload) {
-    return this.execute((http) => {
-      return http.post('',
-        payload.getParams(),
-        http.getConfigBuilder()
-          .setTimeout(this.repositoryClientConfig.readTimeout)
-          .setResponseType('stream')
-          .addAcceptHeader(payload.getResponseType())
-          .addContentTypeHeader(payload.getContentType())
-          .get());
-    }).then((response) => {
-      return response.data;
-    });
-  }
-
-  /**
    * Parses provided content with registered parser if there is one. Otherwise
    * returns the content untouched. If <code>contentType</code> is provided it
    * should be an instance of {@link RDFMimeType} enum and is used as a key
@@ -177,111 +96,6 @@ class BaseRepositoryClient {
     }
     const parser = this.parserRegistry.get(responseType);
     return parser.parse(content);
-  }
-
-  /**
-   * Saves the provided statement payload in the repository.
-   *
-   * The payload will be converted to a quad or a collection of quads in case
-   * there are multiple contexts.
-   *
-   * After the conversion, the produced quad(s) will be serialized to Turtle
-   * format and send to the repository as payload.
-   *
-   * See {@link #addQuads()}.
-   *
-   * @param {Object} payload params holding request parameters as returned
-   *                 by {@link AddStatementPayload#get()}
-   * @return {Promise} promise that will be resolved if the addition is
-   *                    successful or rejected in case of failure
-   */
-  add(payload) {
-    if (!payload) {
-      throw new Error('Cannot add statement without payload');
-    }
-
-    const subject = payload.subject;
-    const predicate = payload.predicate;
-    const object = payload.object;
-    const context = payload.context;
-
-    if (BaseRepositoryClient.hasNullArguments(subject, predicate, object)) {
-      throw new Error('Cannot add statement with null ' +
-        'subject, predicate or object');
-    }
-
-    let quads;
-    if (payload.language) {
-      quads = TermConverter.getQuadsWithLanguage(subject, predicate, object,
-        payload.language, context);
-    } else if (payload.dataType) {
-      quads = TermConverter.getQuadsWithDataType(subject, predicate, object,
-        payload.dataType, context);
-    } else {
-      quads = TermConverter.getQuads(subject, predicate, object, context);
-    }
-
-    return this.addQuads(quads);
-  }
-
-  /**
-   * Serializes the provided quads to Turtle format and sends them to the
-   * repository as payload.
-   *
-   * @param {Quad[]} quads collection of quads to be sent as Turtle text
-   * @return {Promise} promise that will be resolved if the addition is
-   *                    successful or rejected in case of failure
-   */
-  addQuads(quads) {
-    return TermConverter.toTurtle(quads).then((payload) => {
-      return this.execute((http) => http.post(PATH_STATEMENTS, payload,
-        http.getConfigBuilder()
-          .setTimeout(this.repositoryClientConfig.writeTimeout)
-          .addContentTypeHeader(RDFMimeType.TURTLE)
-          .get()));
-    });
-  }
-
-  /**
-   * Deletes statements in the repository based on the provided subject,
-   * predicate, object and or contexts. Each of them is optional and acts as
-   * statements filter which effectively narrows the scope of the deletion.
-   *
-   * Providing context or contexts will restricts the operation to one or more
-   * specific contexts in the repository.
-   *
-   * @param {String} [subject] N-Triples encoded resource subject
-   * @param {String} [predicate] N-Triples encoded resource predicate
-   * @param {String} [object] N-Triples encoded resource object
-   * @param {String[]|String} [contexts] N-Triples encoded resource or resources
-   * @return {Promise} promise that will be resolved if the deletion is
-   *                         successful or rejected in case of failure
-   */
-  deleteStatements(subject, predicate, object, contexts) {
-    return this.execute((http) => http.deleteResource(PATH_STATEMENTS,
-      http.getConfigBuilder()
-        .setTimeout(this.repositoryClientConfig.writeTimeout)
-        .setParams({
-          subj: subject,
-          pred: predicate,
-          obj: object,
-          context: contexts
-        })
-        .get()
-    ));
-  }
-
-  /**
-   * Deletes all statements in the repository.
-   *
-   * @return {Promise} promise that will be resolved if the deletion is
-   *                   successful or rejected in case of failure
-   */
-  deleteAllStatements() {
-    return this.execute((http) => http.deleteResource(PATH_STATEMENTS,
-      http.getConfigBuilder()
-        .setTimeout(this.repositoryClientConfig.writeTimeout)
-        .get()));
   }
 
   /**
@@ -365,17 +179,6 @@ class BaseRepositoryClient {
       throw new Error('Cannot instantiate a repository without repository '
         + 'endpoint configuration! At least one endpoint must be provided.');
     }
-  }
-
-  /**
-   * Checks if at least one of the supplied arguments is undefined or null.
-   *
-   * @private
-   * @return {boolean} <code>true</code> if there is null argument or
-   *         <code>false</code> otherwise
-   */
-  static hasNullArguments(...args) {
-    return [...args].some((arg) => !arg);
   }
 }
 
--- src/repository/rdf-repository-client.js
+++ src/repository/rdf-repository-client.js
@@ -4,7 +4,7 @@ const DataFactory = require('n3').DataFactory;
 const NamedNode = DataFactory.internal.NamedNode;
 const Namespace = require('model/namespace');
 const StringUtils = require('util/string-utils');
-
+const TermConverter = require('model/term-converter');
 const RepositoryClientConfig = require('repository/repository-client-config');
 const TransactionalRepositoryClient =
   require('transaction/transactional-repository-client');
@@ -17,6 +17,13 @@ const TransactionalRepositoryClient =
 const PATH_NAMESPACES = '/namespaces';
 
 /**
+ * Defines a path segment for statements rest endpoint
+ *
+ * @type {string}
+ */
+const PATH_STATEMENTS = '/statements';
+
+/**
  * RDF repository client implementation realizing specific operations.
  * @class
  */
@@ -26,6 +33,26 @@ class RDFRepositoryClient extends BaseRepositoryClient {
    */
   constructor(repositoryClientConfig) {
     super(repositoryClientConfig);
+  }
+
+  /**
+   * Retrieves the size of the repository.
+   *
+   * Effectively returns how much statements are in the repository.
+   *
+   * If one or multiple context are provided, the operation will be restricted
+   * upon each of them.
+   *
+   * @param {string|string[]} [context] context or contexts to restrict the
+   *                            size calculation
+   * @return {Promise<number>} a promise resolving to the total number of
+   *                           statements in the repository
+   */
+  getSize(context) {
+    return this.execute((http) => http.get('/size', http.getConfigBuilder()
+      .setTimeout(this.repositoryClientConfig.readTimeout)
+      .setParams({context})
+      .get())).then((response) => response.data);
   }
 
   /**
@@ -143,6 +170,163 @@ class RDFRepositoryClient extends BaseRepositoryClient {
         .setTimeout(this.repositoryClientConfig.writeTimeout)
         .get()
     ));
+  }
+
+  /**
+   * Fetch rdf data from statements endpoint using provided parameters.
+   *
+   * @param {Object} params is an object holding request parameters as returned
+   *                 by {@link GetStatementsPayload#get()}
+   * @return {Promise<string|Quad>} resolves with plain string or Quad according
+   *      to provided response type.
+   */
+  get(params) {
+    return this.execute((http) => {
+      return http.get(PATH_STATEMENTS, http.getConfigBuilder()
+        .setParams({
+          subj: params.subject,
+          pred: params.predicate,
+          obj: params.object,
+          context: params.context,
+          infer: params.inference
+        })
+        .addAcceptHeader(params.responseType)
+        .setTimeout(this.repositoryClientConfig.readTimeout)
+        .get());
+    }).then((response) => {
+      return this.parse(response.data, params.responseType);
+    });
+  }
+
+  /**
+   * Executes request to query a repository.
+   *
+   * Only POST request with a valid QueryPayload is supported.
+   *
+   * @param {QueryPayload} payload is an object holding request parameters
+   * required by the query POST endpoint.
+   * @return {Promise} the client can subscribe to the stream events and consume
+   * the emitted strings or Quads depending on the provided response type as
+   * soon as they are available.
+   */
+  query(payload) {
+    return this.execute((http) => {
+      return http.post('',
+        payload.getParams(),
+        http.getConfigBuilder()
+          .setTimeout(this.repositoryClientConfig.readTimeout)
+          .setResponseType('stream')
+          .addAcceptHeader(payload.getResponseType())
+          .addContentTypeHeader(payload.getContentType())
+          .get());
+    }).then((response) => {
+      return response.data;
+    });
+  }
+
+  /**
+   * Saves the provided statement payload in the repository.
+   *
+   * The payload will be converted to a quad or a collection of quads in case
+   * there are multiple contexts.
+   *
+   * After the conversion, the produced quad(s) will be serialized to Turtle
+   * format and send to the repository as payload.
+   *
+   * See {@link #addQuads()}.
+   *
+   * @param {Object} payload params holding request parameters as returned
+   *                 by {@link AddStatementPayload#get()}
+   * @return {Promise} promise that will be resolved if the addition is
+   *                    successful or rejected in case of failure
+   */
+  add(payload) {
+    if (!payload) {
+      throw new Error('Cannot add statement without payload');
+    }
+
+    const subject = payload.subject;
+    const predicate = payload.predicate;
+    const object = payload.object;
+    const context = payload.context;
+
+    if (RDFRepositoryClient.hasNullArguments(subject, predicate, object)) {
+      throw new Error('Cannot add statement with null ' +
+        'subject, predicate or object');
+    }
+
+    let quads;
+    if (payload.language) {
+      quads = TermConverter.getQuadsWithLanguage(subject, predicate, object,
+        payload.language, context);
+    } else if (payload.dataType) {
+      quads = TermConverter.getQuadsWithDataType(subject, predicate, object,
+        payload.dataType, context);
+    } else {
+      quads = TermConverter.getQuads(subject, predicate, object, context);
+    }
+
+    return this.addQuads(quads);
+  }
+
+  /**
+   * Serializes the provided quads to Turtle format and sends them to the
+   * repository as payload.
+   *
+   * @param {Quad[]} quads collection of quads to be sent as Turtle text
+   * @return {Promise} promise that will be resolved if the addition is
+   *                    successful or rejected in case of failure
+   */
+  addQuads(quads) {
+    return TermConverter.toTurtle(quads).then((payload) => {
+      return this.execute((http) => http.post(PATH_STATEMENTS, payload,
+        http.getConfigBuilder()
+          .setTimeout(this.repositoryClientConfig.writeTimeout)
+          .addContentTypeHeader(RDFMimeType.TURTLE)
+          .get()));
+    });
+  }
+
+  /**
+   * Deletes statements in the repository based on the provided subject,
+   * predicate, object and or contexts. Each of them is optional and acts as
+   * statements filter which effectively narrows the scope of the deletion.
+   *
+   * Providing context or contexts will restricts the operation to one or more
+   * specific contexts in the repository.
+   *
+   * @param {String} [subject] N-Triples encoded resource subject
+   * @param {String} [predicate] N-Triples encoded resource predicate
+   * @param {String} [object] N-Triples encoded resource object
+   * @param {String[]|String} [contexts] N-Triples encoded resource or resources
+   * @return {Promise} promise that will be resolved if the deletion is
+   *                         successful or rejected in case of failure
+   */
+  deleteStatements(subject, predicate, object, contexts) {
+    return this.execute((http) => http.deleteResource(PATH_STATEMENTS,
+      http.getConfigBuilder()
+        .setTimeout(this.repositoryClientConfig.writeTimeout)
+        .setParams({
+          subj: subject,
+          pred: predicate,
+          obj: object,
+          context: contexts
+        })
+        .get()
+    ));
+  }
+
+  /**
+   * Deletes all statements in the repository.
+   *
+   * @return {Promise} promise that will be resolved if the deletion is
+   *                   successful or rejected in case of failure
+   */
+  deleteAllStatements() {
+    return this.execute((http) => http.deleteResource(PATH_STATEMENTS,
+      http.getConfigBuilder()
+        .setTimeout(this.repositoryClientConfig.writeTimeout)
+        .get()));
   }
 
   /**
@@ -293,6 +477,17 @@ class RDFRepositoryClient extends BaseRepositoryClient {
     const config = this.repositoryClientConfig;
     return new RepositoryClientConfig([locationUrl], config.headers,
       config.defaultRDFMimeType, config.readTimeout, config.writeTimeout);
+  }
+
+  /**
+   * Checks if at least one of the supplied arguments is undefined or null.
+   *
+   * @private
+   * @return {boolean} <code>true</code> if there is null argument or
+   *         <code>false</code> otherwise
+   */
+  static hasNullArguments(...args) {
+    return [...args].some((arg) => !arg);
   }
 }
 
