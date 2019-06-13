a4a11733165553d90c121828718c74ac634fa89c
Svilen Velikov <svilen.velikov@sirma.bg>
2019-06-13 09:19:02 +0300

    Moved getLogPayload in ConsoleLogger to allow reusing and updated classes where it was used

--- src/logging/console-logger.js
+++ src/logging/console-logger.js
@@ -77,6 +77,28 @@ class ConsoleLogger extends Logger {
   trace(...args) {
     this.logger.trace(...args);
   }
+
+  /**
+   * Creates an object from the provided HTTP response that is suitable for
+   * structured logging.
+   *
+   * Any additional key-value entries from <code>params</code> will be assigned
+   * in the created payload object.
+   *
+   * @static
+   * @param {HttpResponse} response the HTTP response.
+   * Used to get the execution time and the base URL
+   * @param {object} [params] additional parameters to be appended
+   * @return {object} the constructed payload object for logging
+   */
+  static getLogPayload(response, params = {}) {
+    const payload = {
+      elapsedTime: response.getElapsedTime(),
+      repositoryUrl: response.getBaseURL()
+    };
+    Object.assign(payload, params);
+    return payload;
+  }
 }
 
 module.exports = ConsoleLogger;
