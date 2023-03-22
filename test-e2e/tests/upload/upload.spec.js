88dfb853959c6c08baf5917a41eebe2ef6717928
Svilen Velikov <svilen.velikov@ontotext.com>
2023-03-22 08:32:57 +0200

    Refactored upload tests. Also removed redundant namespaces tests file which was previously split and moved in its own package.

--- test-e2e/tests/upload/upload.spec.js
+++ test-e2e/tests/upload/upload.spec.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const {RDFRepositoryClient, GetStatementsPayload} = require('graphdb').repository;
 const {RDFMimeType} = require('graphdb').http;
-const Config = require('config');
-const Utils = require('utils');
+const Config = require('config.js');
+const Utils = require('utils.js');
 
-describe('Should upload and overwrite data', () => {
+describe('Upload data', () => {
 
   beforeAll(() => {
     return Utils.createRepo(Config.testRepoPath);
@@ -16,7 +16,7 @@ describe('Should upload and overwrite data', () => {
 
   let rdfClient = new RDFRepositoryClient(Config.restApiConfig);
 
-  test('Should upload data', () => {
+  test('Should upload and overwrite data', () => {
     let params = new GetStatementsPayload()
       .setResponseType(RDFMimeType.RDF_JSON)
       .setSubject('<http://learningsparql.com/ns/data/i0432>')
@@ -26,13 +26,13 @@ describe('Should upload and overwrite data', () => {
     let expectedResponse = Utils.loadFile('./data/upload_overwrite/expected_response.json');
     let expectedResponseOverwritten = Utils.loadFile('./data/upload_overwrite/expected_response_overwritten.json');
 
-    let sampleRdf = path.resolve(__dirname, './data/sample-turtle.ttl');
+    let sampleRdf = path.resolve(__dirname, './../data/sample-turtle.ttl');
     let turtleStream = Utils.getReadStream(sampleRdf);
-    let context = '<http://domain/graph/data-graph-3>';
 
-    let sampleRdfEdited = path.resolve(__dirname, './data/sample-turtle_edited.ttl');
+    let sampleRdfEdited = path.resolve(__dirname, './../data/sample-turtle_edited.ttl');
     let turtleStreamEdited = Utils.getReadStream(sampleRdfEdited);
 
+    let context = '<http://domain/graph/data-graph-3>';
     return rdfClient.upload(turtleStream, RDFMimeType.TURTLE, context, null).then(() => {
       return rdfClient.get(params);
     }).then((resp) => {
