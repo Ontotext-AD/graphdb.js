0352b53345d06f1c7297192a74651a36212cf80e
svilen.velikov <svilen.velikov@ontotext.com>
2025-03-20 19:25:51 +0200

    trying with test containers

--- test-e2e/tests/config.js
+++ test-e2e/tests/config.js
@@ -4,41 +4,81 @@ const {ServerClientConfig} = require('graphdb').server;
 const {RepositoryClientConfig} = require('graphdb').repository;
 
 // Variables used in tests
-const serverAddress = process.env.GRAPHDB_BASE_URL || 'http://localhost:7200';
+let serverAddress = process.env.GRAPHDB_BASE_URL || 'http://localhost:7200';
 const testRepoPath = './tests/data/repositories/GDB_Free/repository1.ttl';
 const testRepo2Path = './tests/data/repositories/GDB_Free/repository2.ttl';
 
-const restApiConfig = new RepositoryClientConfig(serverAddress)
-  .setEndpoints([`${serverAddress}/repositories/Test_repo`])
-  .setHeaders({
-    'Accept': RDFMimeType.SPARQL_RESULTS_XML
-  });
+// Initialize empty configs that will be populated later
+let restApiConfig;
+let restApiBasicAuthConfig;
+let restApiTokenAuthConfig;
+let serverBasicAuthConfig;
+let serverConfig;
 
-const restApiBasicAuthConfig = new RepositoryClientConfig(serverAddress)
-  .setEndpoints([`${serverAddress}/repositories/Test_repo`])
-  .setHeaders({
-    'Accept': RDFMimeType.SPARQL_RESULTS_XML
-  })
-  .useBasicAuthentication('admin', 'root');
+// Function to initialize configs with current serverAddress
+const initializeConfigs = () => {
+  restApiConfig = new RepositoryClientConfig(serverAddress)
+    .setEndpoints([`${serverAddress}/repositories/Test_repo`])
+    .setHeaders({
+      'Accept': RDFMimeType.SPARQL_RESULTS_XML
+    });
 
-const restApiTokenAuthConfig = new RepositoryClientConfig(serverAddress)
-  .setEndpoints([`${serverAddress}/repositories/Test_repo`])
-  .setHeaders({
-    'Accept': RDFMimeType.SPARQL_RESULTS_XML
-  })
-  .useGdbTokenAuthentication('admin', 'root');
+  restApiBasicAuthConfig = new RepositoryClientConfig(serverAddress)
+    .setEndpoints([`${serverAddress}/repositories/Test_repo`])
+    .setHeaders({
+      'Accept': RDFMimeType.SPARQL_RESULTS_XML
+    })
+    .useBasicAuthentication('admin', 'root');
 
+  restApiTokenAuthConfig = new RepositoryClientConfig(serverAddress)
+    .setEndpoints([`${serverAddress}/repositories/Test_repo`])
+    .setHeaders({
+      'Accept': RDFMimeType.SPARQL_RESULTS_XML
+    })
+    .useGdbTokenAuthentication('admin', 'root');
 
-const serverBasicAuthConfig = new ServerClientConfig(serverAddress)
-  .setHeaders({
-    'Accept': RDFMimeType.SPARQL_RESULTS_JSON
-  })
-  .useBasicAuthentication('admin', 'root');
+  serverBasicAuthConfig = new ServerClientConfig(serverAddress)
+    .setHeaders({
+      'Accept': RDFMimeType.SPARQL_RESULTS_JSON
+    })
+    .useBasicAuthentication('admin', 'root');
 
-const serverConfig = new ServerClientConfig(serverAddress)
-  .setHeaders({
-    'Accept': RDFMimeType.SPARQL_RESULTS_JSON
-  });
+  serverConfig = new ServerClientConfig(serverAddress)
+    .setHeaders({
+      'Accept': RDFMimeType.SPARQL_RESULTS_JSON
+    });
+};
 
-module.exports = {restApiConfig, serverAddress, testRepoPath, testRepo2Path,
-  restApiBasicAuthConfig, restApiTokenAuthConfig, serverBasicAuthConfig, serverConfig};
+const updateServerAddress = (newAddress) => {
+  // ESLint warning can be fixed by using console.info or adding /* eslint-disable no-console */
+  console.info(`Updating server address to: ${newAddress}`);
+  serverAddress = newAddress;
+  initializeConfigs(); // Recreate all configs with the new address
+};
+
+// Initialize with default address
+initializeConfigs();
+
+module.exports = {
+  get restApiConfig() {
+    return restApiConfig;
+  },
+  get restApiBasicAuthConfig() {
+    return restApiBasicAuthConfig;
+  },
+  get restApiTokenAuthConfig() {
+    return restApiTokenAuthConfig;
+  },
+  get serverBasicAuthConfig() {
+    return serverBasicAuthConfig;
+  },
+  get serverConfig() {
+    return serverConfig;
+  },
+  get serverAddress() {
+    return serverAddress;
+  },
+  testRepoPath,
+  testRepo2Path,
+  updateServerAddress
+};
